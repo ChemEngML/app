8780731532ae9dd02a19f9fc5220babd6a35be2c
ChemEngML <132061058+ChemEngML@users.noreply.github.com>
2023-06-01 11:00:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,30 +14,7 @@ model = load_model('GradientBoostingRegressor')
 # Import dataset & choosing desired parameters to analyze
 df = pd.read_csv("Database.csv")
 
-# Defining app's characteristics
-def app():
-    st.title('🌊 Mem-brain 🧠')
-    st.header('Water flux (Jw) Predictor')
-    st.subheader('⬅️ Please enter your input values from the sidebar')
 
-    MP_Type = df["Micro Pollutant"].unique()
-    MB_Type = df["Type of MB"].unique()
-    DS_Type = df["Draw Solution"].unique()
-
-    MP = st.sidebar.selectbox("Micropollutant:", MP_Type)
-    MB = st.sidebar.selectbox("Membrane Type:", MB_Type)
-    MP_Conc = st.sidebar.number_input("MP Concentration (mg/L):", 0.0, 100.0, format="%.7f")
-    MP_MW = st.sidebar.number_input("MP Molecular Weight (g/mol):", 0.0, 1000.0, format="%.2f")
-    Charge = st.sidebar.slider("MP Charge:", -1, 1)
-    FS_pH = st.sidebar.slider("FS pH:", 0.0, 14.0, step=0.05)
-    Contact_angle = st.sidebar.number_input("MB Contact Angle (°):", 0.0, 110.0, format="%.2f")
-    DS = st.sidebar.selectbox("Draw Solute:", DS_Type)
-    DS_MW = st.sidebar.number_input("DS Molecular Weight (g/mol):", 0, 999)
-    DS_Conc = st.sidebar.number_input("DS Concentration (M):", 0, 99)
-    Op_Time = st.sidebar.number_input("Operation Time (h):", 0, 72)
-    Velocity = st.sidebar.number_input("Cross-Flow Velocity (cm/s):", 0.0, 50.0, format="%.2f")
-    Temp = st.sidebar.slider("Temperature (⁰C)", 0.0, 40.0, step=0.5)
-    Rejection = st.sidebar.slider("Rejection Rate (%)", 0, 100)
     
 # Define the predict function
 def predict(MP, MB, MP_Conc, MP_MW, Charge, FS_pH, Contact_angle, DS, DS_MW, DS_Conc, Op_Time, Velocity, Temp, Rejection):
@@ -62,7 +39,30 @@ def predict(MP, MB, MP_Conc, MP_MW, Charge, FS_pH, Contact_angle, DS, DS_MW, DS_
     prediction = predict_model(model, input_df)
     st.write("The predicted water flux is:", prediction)
 
+# Defining app's characteristics
+def app():
+    st.title('🌊 Mem-brain 🧠')
+    st.header('Water flux (Jw) Predictor')
+    st.subheader('⬅️ Please enter your input values from the sidebar')
 
+    MP_Type = df["Micro Pollutant"].unique()
+    MB_Type = df["Type of MB"].unique()
+    DS_Type = df["Draw Solution"].unique()
+
+    MP = st.sidebar.selectbox("Micropollutant:", MP_Type)
+    MB = st.sidebar.selectbox("Membrane Type:", MB_Type)
+    MP_Conc = st.sidebar.number_input("MP Concentration (mg/L):", 0.0, 100.0, format="%.7f")
+    MP_MW = st.sidebar.number_input("MP Molecular Weight (g/mol):", 0.0, 1000.0, format="%.2f")
+    Charge = st.sidebar.slider("MP Charge:", -1, 1)
+    FS_pH = st.sidebar.slider("FS pH:", 0.0, 14.0, step=0.05)
+    Contact_angle = st.sidebar.number_input("MB Contact Angle (°):", 0.0, 110.0, format="%.2f")
+    DS = st.sidebar.selectbox("Draw Solute:", DS_Type)
+    DS_MW = st.sidebar.number_input("DS Molecular Weight (g/mol):", 0, 999)
+    DS_Conc = st.sidebar.number_input("DS Concentration (M):", 0, 99)
+    Op_Time = st.sidebar.number_input("Operation Time (h):", 0, 72)
+    Velocity = st.sidebar.number_input("Cross-Flow Velocity (cm/s):", 0.0, 50.0, format="%.2f")
+    Temp = st.sidebar.slider("Temperature (⁰C)", 0.0, 40.0, step=0.5)
+    Rejection = st.sidebar.slider("Rejection Rate (%)", 0, 100)
 
 trigger = st.button('Predict', on_click=predict)
 
